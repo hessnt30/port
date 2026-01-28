cd5d5b647f72a9375a147f2f2b14a46a3f653b8f
Nicholas Hess <114044339+hessnt30@users.noreply.github.com>
2026-01-28 15:59:38 -0500

    Refactor Home component layout and structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,18 @@ export default function Home() {
   const comingSoon = Array(300).fill("COMING SOON");
 
   return (
-    <div className="relative flex h-screen w-screen justify-center items-center overflow-hidden bg-black">
-      <main className="relative z-10">
-        {/* Background */}
-        <div className="absolute inset-0 top-0 left-0 h-screen w-screen z-0 grid grid-cols-6 md:grid-cols-10 lg:grid-cols-14 gap-4 opacity-30 text-gray-300 text-sm md:text-base font-bold select-none pointer-events-none">
-          {comingSoon.map((item, index) => (
-            <span key={index}>{item}</span>
-          ))}
-        </div>
+    <div className="relative h-screen w-screen overflow-hidden bg-black flex items-center justify-center">
+      
+      {/* Background */}
+      <div className="absolute inset-0 z-0 grid grid-cols-6 md:grid-cols-10 lg:grid-cols-14 gap-4 opacity-30 text-gray-300 text-sm md:text-base font-bold select-none pointer-events-none">
+        {comingSoon.map((item, index) => (
+          <span key={index}>{item}</span>
+        ))}
+      </div>
 
-        {/* Foreground */}
-        <div className="relative z-10 font-bold px-12 py-8 bg-black">
+      {/* Foreground */}
+      <main className="relative z-10">
+        <div className="font-bold px-12 py-8 bg-black">
           <h1 className="text-4xl md:text-6xl text-white text-center">
             COMING SOON
           </h1>
@@ -21,6 +22,7 @@ export default function Home() {
           </p>
         </div>
       </main>
+
     </div>
   );
 }
