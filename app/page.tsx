1c477d8ff65a87f0addf1ed6b95e415535c6041a
Nicholas Hess <114044339+hessnt30@users.noreply.github.com>
2026-01-28 15:40:48 -0500

    Modify comingSoon array size and update styles
    Reduced the size of the comingSoon array and updated HTML elements for better styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 export default function Home() {
-  const comingSoon = Array(1200).fill("COMING SOON");
+  const comingSoon = Array(300).fill("COMING SOON");
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center overflow-hidden bg-black">
       <main>
-        <div className="flex flex-wrap text-gray-100 absolute top-0 left-0 z-0">
+        <div className="absolute inset-0 z-0 grid grid-cols-6 md:grid-cols-10 gap-6 opacity-10 text-gray-300 text-sm md:text-base font-bold select-none">
           {comingSoon.map((item, index) => (
-            <p key={index}>{item}</p>
+            <span key={index}>{item}</span>
           ))}
         </div>
 
-        <div className="z-10 relative font-bold px-10 py-6 bg-background">
-          <h1 className="text-4xl md:text-6xl">COMING SOON</h1>
-          <p className="text-center md:text-2xl">nicholashess0129@gmail.com</p>
+        <div className="z-10 relative font-bold px-10 py-6 bg-black">
+          <h1 className="text-4xl md:text-6xl text-white">COMING SOON</h1>
+          <p className="text-center md:text-2xl text-white">nicholashess0129@gmail.com</p>
         </div>
       </main>
     </div>
